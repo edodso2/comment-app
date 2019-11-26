4611b122ff9634ccf2bc901eb6062617ad0da76b
edodso2 <2.3turbofox@gmail.com>
2019-11-25 21:31:03 -0500

    Add build script

---
+++ build/build.js
@@ -0,0 +1,10 @@
+const concat = require('concat');
+
+(async function build() {
+  const files = [
+    './dist/0.app.js',
+    './dist/app.js'
+  ];
+
+  await concat(files, 'dist/tfsCommentApp.js');
+})();

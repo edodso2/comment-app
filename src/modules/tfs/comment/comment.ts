00dc348f72f998d8adafe47009bcbc2f17a10fd6
Sean Dodson <dodsonsean@comcast.net>
2020-02-29 14:53:51 -0500

    Move like to separate component

--- src/modules/tfs/comment/comment.ts
+++ src/modules/tfs/comment/comment.ts
@@ -14,63 +14,11 @@ export default class Comment extends LightningElement {
   @api comment: CommentObj;
   @api currentUser: User;
 
-  @track isLiked = false;
-  @track likeCount = 0;
-
-  connectedCallback() {
-    // these are not calculated getters so that we can manually and immediately
-    // set them when the user clicks the like/unlike button
-    this.isLiked =
-      this.currentUser &&
-      this.comment.likes.some(u => u.uid === this.currentUser.uid);
-    this.likeCount = this.comment.likes.length;
-  }
-
   get commentDate() {
     return this.comment.date.toLocaleDateString();
   }
 
   handleImgError() {
     this.showPlaceholderImg = true;
-  }
-
-  likeComment() {
-    if (this._canLike()) {
-      // mark commment as liked
-      this.isLiked = true;
-      this.likeCount++;
-
-      // raise liked event
-      const likedEvent = new CustomEvent('likecomment', {
-        detail: { comment: this.comment, userId: this.currentUser.uid },
-        bubbles: true,
-        cancelable: true,
-        composed: true
-      });
-
-      this.dispatchEvent(likedEvent);
-    }
-  }
-
-  unlikeComment() {
-    if (this._canLike()) {
-      // remove comment as liked
-      this.isLiked = false;
-      this.likeCount--;
-
-      // raise unliked event
-      const unlikedEvent = new CustomEvent('unlikecomment', {
-        detail: { comment: this.comment, userId: this.currentUser.uid },
-        bubbles: true,
-        cancelable: true,
-        composed: true
-      });
-
-      this.dispatchEvent(unlikedEvent);
-    }
-  }
-
-  _canLike() {
-    return this.currentUser;
   }
 }
